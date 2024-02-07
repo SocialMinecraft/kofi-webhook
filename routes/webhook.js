3a11c5884823a15606b25e6e1fe979b15709f30a
Dylan Garvis <dylan@garvis.dev>
2024-02-07 16:05:59 -0500

    Adjusted field parameter.

--- routes/webhook.js
+++ routes/webhook.js
@@ -15,7 +15,7 @@ var Singleton = require('../singleton');
 });*/
 
 router.post('/', async (req, res) => {
-    const body = req.body;
+    const body = req.body.data;
 
     // Check the verification token
     if (Singleton.config.kofiVerification != body.verification_token) {
