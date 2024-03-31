41d439f2cf361cc8e1b7b2357517df45abf11b6d
Dylan Garvis <dylan@garvis.dev>
2024-03-30 23:08:26 -0400

    and the protobuf package.

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var protobuf = require("protobufjs");
 
 var bodyParser = require('body-parser');
 router.use(bodyParser.json()); // support json encoded bodies
